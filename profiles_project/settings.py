3b1163746e476c11e97bdf68a4d06c22948725a9
mantu0314 <63896537+mantu0314@users.noreply.github.com>
2020-08-12 18:45:22 +0530

    Update settings.py

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,8 +121,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mantu838@gmail.com'
-EMAIL_HOST_PASSWORD ='mK123456'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
